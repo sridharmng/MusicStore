9e2ee8867b97f0b34fc39cc34689a20351405c41
sridharmng <sri.mangalampalli@gmail.com>
2016-01-15 14:31:34 +0530

    one

---
+++ MusicStoreApp/Controllers/StoreController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStoreApp.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+        public string Index()
+        {
+            return"Hello from Store.Index()";
+        }
+        public string Browse(string genre)
+        {
+            string message = HttpUtility.HtmlEncode("Store.Browse,Genre= " + genre);
+            return message;
+        }
+        public string Details(int id)
+        {
+            string message = "store.Details,Id= " + id;
+            return message;
+        }
+	}
+}
