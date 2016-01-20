b5d69a56d29cc439fb06edb9d15ea923a2793dc6
sridharmng <sri.mangalampalli@gmail.com>
2016-01-20 17:26:24 +0530

    commit

---
+++ MusicStoreApp/Models/Album.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicStoreApp.Models
+{
+    public class Album
+    {
+        public string Title { get; set; }
+        public Genre Genre {get;set;}
+        
+    }
+}
